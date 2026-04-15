8c17cfd3cd71c21ef6094750dfc46c6d237253a5
Ajit Dev <ajitk23192@gmail.com>
2026-04-15 22:06:37 +0530

    🚀 Product of Array Except Self (C++)  An optimized C++ solution to compute an array where each element is the product of all other elements except itself, without using division. This approach uses prefix and suffix products to achieve O(n) time complexity and O(1) extra space, making it efficient for large inputs.
    🚀 Product of Array Except Self (C++)
    
    An optimized C++ solution to compute an array where each element is the product of all other elements except itself, without using division. This approach uses prefix and suffix products to achieve O(n) time complexity and O(1) extra space, making it efficient for large inputs.

---
+++ 238. Product of Array Except Self.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> productExceptSelf(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+
+        // prefix
+        for (int i = 1; i < n; i++)
+        {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+
+        // suffix
+        int sufix = 1;
+        for (int i = n - 2; i >= 0; i--)
+        {
+            sufix *= nums[i + 1];
+            ans[i] *= sufix;
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+}
