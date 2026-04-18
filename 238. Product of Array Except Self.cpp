0f8f237e9615f5059bd81b6a8c31508cfad38ca9
Ajit Dev <ajitk23192@gmail.com>
2026-04-18 14:18:54 +0530

    Add productExceptSelf function implementation
    Implemented the product of array except self algorithm using prefix and suffix products.

--- 238. Product of Array Except Self.cpp
+++ 238. Product of Array Except Self.cpp
@@ -8,13 +8,11 @@ public:
     {
         int n = nums.size();
         vector<int> ans(n, 1);
-
         // prefix
         for (int i = 1; i < n; i++)
         {
             ans[i] = ans[i - 1] * nums[i - 1];
         }
-
         // suffix
         int sufix = 1;
         for (int i = n - 2; i >= 0; i--)
@@ -22,11 +20,6 @@ public:
             sufix *= nums[i + 1];
             ans[i] *= sufix;
         }
-
         return ans;
     }
 };
-
-int main()
-{
-}
