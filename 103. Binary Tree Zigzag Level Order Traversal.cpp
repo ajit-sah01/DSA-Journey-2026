beca8b7f3109cd128806c7670e5e37c846573c46
Ajit Dev <ajitk23192@gmail.com>
2026-04-28 11:08:20 +0530

    Fix formatting and remove unnecessary whitespace

--- 103. Binary Tree Zigzag Level Order Traversal.cpp
+++ 103. Binary Tree Zigzag Level Order Traversal.cpp
@@ -25,7 +25,6 @@ public:
             for (int i = 0; i < size; ++i) {
                 TreeNode* node = q.front();
                 q.pop();
-
                 // Determine position based on current direction
                 int index = leftToRight ? i : (size - 1 - i);
                 level[index] = node->val;
@@ -37,7 +36,6 @@ public:
             ans.push_back(std::move(level));
             leftToRight = !leftToRight; // Flip the direction for the next level
         }
-
         return ans;
     }
 };
