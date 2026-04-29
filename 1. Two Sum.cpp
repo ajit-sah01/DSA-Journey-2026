d87b730023d386ac31eae2c3238b6ff641c175d3
Ajit Kumar <ajitk23192@gmail.com>
2026-04-29 15:01:17 +0000

    update Two Sum solution

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,15 +1,25 @@
-class Solution {
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-      int n = nums.size();
-        // O(n2) Time Complexity ..
-      for(int i = 0 ; i<n;i++){
-        for(int j = i+1 ; j<n;j++){
-            if(nums[i] + nums[j] == target){
-                return {i,j}; // if found ans return 
-            };
+  vector<int> twoSum(vector<int> &nums, int target)
+  {
+    int n = nums.size();
+    // O(n2) Time Complexity ..
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = i + 1; j < n; j++)
+      {
+        if (nums[i] + nums[j] == target)
+        {
+          return {i, j}; // if found ans return
         };
       };
-      return {-1,-1}; // not find tha ans
     };
+    return {-1, -1}; // not find tha ans
+  };
 };
