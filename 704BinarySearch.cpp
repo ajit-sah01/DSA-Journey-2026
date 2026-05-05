5d707fceeffb464cbfa66cd2dd03e5baae4a45c9
Ajit Dev <ajitk23192@gmail.com>
2026-05-05 08:37:11 +0530

    Update 704BinarySearch.cpp
    Updated

--- 704BinarySearch.cpp
+++ 704BinarySearch.cpp
@@ -1,42 +1,32 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+    int n = nums.size();
 
-int search(vector<int>& nums, int target) {
-    int st = 0;
-    int end = nums.size() - 1;
+    
+    // // O(n) Linear Search 
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (nums[i] == target)
+    //     {
+    //         return i; // target found
+    //     }
+    // }
 
-    while (st <= end) {
-        int mid = st + (end - st) / 2;
 
-        if (nums[mid] == target) {
+    // bin search  O(log n)
+    int st =0 , end = n-1;
+    while(st<=end){ 
+        int mid = st + (end - st) / 2 ;
+        if(target == nums[mid]){
             return mid;
-        } 
-        else if (nums[mid] < target) {
-            st = mid + 1;
-        } 
-        else {
-            end = mid - 1;
+        }else if(nums[mid] < target){
+            st = mid +1;
+        }else{
+            end = mid -1;
         }
     }
 
-    return -1;
-}
-
-int main() {
-    vector<int> nums = {1, 3, 5, 7, 9, 11};
-    int target;
-
-    cout << "Enter target: ";
-    cin >> target;
-
-    int result = search(nums, target);
-
-    if (result != -1) {
-        cout << "Element found at index: " << result << endl;
-    } else {
-        cout << "Element not found" << endl;
+    return -1; // not found
     }
-
-    return 0;
-}
+};
