ca351026b44c6cbed6d8cd8612fef697d88434cf
Ajit Kumar <ajitk23192@gmail.com>
2026-05-10 12:22:35 +0000

    More Otimized code

--- 151. Reverse Words in a String.cpp
+++ 151. Reverse Words in a String.cpp
@@ -1,35 +1,31 @@
-class Solution {
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution
+{
 public:
-    string reverseWords(string s) {
-        // 1. Reverse the whole string
+    string reverseWords(string s)
+    {
+        int n = s.length();
+        string ans = "";
         reverse(s.begin(), s.end());
 
-        int n = s.length();
-        int idx = 0; // The position where we write the "cleaned" characters
-
-        for (int start = 0; start < n; ++start) {
-            if (s[start] != ' ') {
-                // If we are starting a new word (and it's not the first word),
-                // add a single space separator
-                if (idx != 0) s[idx++] = ' ';
-
-                // Find the end of the current word
-                int end = start;
-                while (end < n && s[end] != ' ') {
-                    s[idx++] = s[end++];
-                }
-
-                // Reverse the word we just found back to its original order
-                // We reverse from (idx - length of word) to idx
-                reverse(s.begin() + idx - (end - start), s.begin() + idx);
-
-                // Move start pointer to the end of this word
-                start = end;
+        for (int i = 0; i < n; i++)
+        {
+            string word = "";
+            while (i < n && s[i] != ' ')
+            {
+                word += s[i];
+                i++;
+            }
+            reverse(word.begin(), word.end());
+            if (word.length() > 0)
+            {
+                ans += " " + word;
             }
         }
-
-        // Resize the string to remove the leftover characters at the end
-        s.erase(s.begin() + idx, s.end());
-        return s;
+        return ans.substr(1);
     }
 };
