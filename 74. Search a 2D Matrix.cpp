b2b2bf0b546b6c4edd702d7c4eb98bce2cf3381d
Ajit Kumar <ajitk23192@gmail.com>
2026-05-14 12:22:11 +0000

    Update 74. Search a 2D Matrix

--- 74. Search a 2D Matrix.cpp
+++ 74. Search a 2D Matrix.cpp
@@ -1,26 +1,59 @@
-class Solution {
+#include <iostream>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+//// Binaery Search in 2D Array
+class Solution
+{
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if (matrix.empty() || matrix[0].empty()) return false;
-
-        int m = matrix.size();
+    bool SearchInRow(vector<vector<int>> &matrix, int target, int row) // O(log n)
+    {
         int n = matrix[0].size();
-        
-        int low = 0;
-        int high = m * n - 1;
+        int st = 0, end = n - 1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            
-            // Map 1D index back to 2D coordinates
-            int midValue = matrix[mid / n][mid % n];
+        while (st <= end)
+        {
+            int mid = st + (end - st) / 2;
 
-            if (midValue == target) {
+            if (target == matrix[row][mid])
+            {
                 return true;
-            } else if (midValue < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+            }
+            else if (target > matrix[row][mid])
+            {
+                st = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+    bool searchMatrix(vector<vector<int>> &matrix, int target) // O(log m)
+    {
+        /// Bs on Tot Rows
+        int m = matrix.size(), n = matrix[0].size();
+        int starRow = 0, endRow = m - 1;
+        while (starRow <= endRow)
+        {
+            int midRow = starRow + (endRow - starRow) / 2;
+
+            if (target >= matrix[midRow][0] && target <= matrix[midRow][n - 1])
+            {
+                // found The Row -> next Bs Serach in cols
+                return SearchInRow(matrix, target, midRow);
+            }
+            else if (target > matrix[midRow][n - 1])
+            {
+                // down -> right
+                starRow = midRow + 1;
+            }
+            else
+            {
+                // up -> left
+                endRow = midRow - 1;
             }
         }
 
