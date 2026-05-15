b4032671975f6f2093dae1115bf3e4bee36636ba
Ajit Dev <ajitk23192@gmail.com>
2026-05-15 17:46:42 +0530

    Update With my Logic
    Update With my Logic

--- 54. Spiral Matrix.cpp
+++ 54. Spiral Matrix.cpp
@@ -1,41 +1,51 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> result;
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
 
-        int m = matrix.size();
-        int n = matrix[0].size();
+        int m = mat.size(), n = mat[0].size();
 
-        int top = 0, bottom = m - 1;
-        int left = 0, right = n - 1;
+        int srow = 0, scol = 0;
+        int erow = m - 1, ecol = n - 1;
 
-        while (top <= bottom && left <= right) {
+        vector<int> ans;
 
-            // Traverse top row
-            for (int j = left; j <= right; j++)
-                result.push_back(matrix[top][j]);
-            top++;
+        while (srow <= erow && scol <= ecol) {
 
-            // Traverse right column
-            for (int i = top; i <= bottom; i++)
-                result.push_back(matrix[i][right]);
-            right--;
-
-            // Traverse bottom row (if valid)
-            if (top <= bottom) {
-                for (int j = right; j >= left; j--)
-                    result.push_back(matrix[bottom][j]);
-                bottom--;
+            // top
+            for (int i = scol; i <= ecol; i++) {
+                ans.push_back(mat[srow][i]);
             }
 
-            // Traverse left column (if valid)
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--)
-                    result.push_back(matrix[i][left]);
-                left++;
+            // right
+            for (int j = srow + 1; j <= erow; j++) {
+                ans.push_back(mat[j][ecol]);
             }
+
+            // bottom
+            for (int k = ecol - 1; k >= scol; k--) {
+
+                if (srow == erow)
+                    break;
+
+                ans.push_back(mat[erow][k]);
+            }
+
+            // left
+            for (int l = erow - 1; l >= srow + 1; l--) {
+
+                if (scol == ecol)
+                    break;
+
+                ans.push_back(mat[l][scol]);
+            }
+
+            srow++;
+            scol++;
+
+            erow--;
+            ecol--;
         }
 
-        return result;
+        return ans;
     }
 };
