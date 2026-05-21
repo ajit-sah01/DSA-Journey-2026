cb735d460f4e1c898b8cc70741c861fb9f6639ec
Ajit Dev <ajitk23192@gmail.com>
2026-05-21 18:54:33 +0530

    Replace backtrack with GetallSubsets for subsets
    Refactor subset generation to use GetallSubsets method.

--- 78. Subsets.cpp
+++ 78. Subsets.cpp
@@ -1,22 +1,30 @@
-class Solution {
-public:
-    void backtrack(vector<int>& nums, int start, vector<int>& current, vector<vector<int>>& result) {
-        // Add the current subset to the result (including the empty set)
-        result.push_back(current);
 
-        for (int i = start; i < nums.size(); ++i) {
-            current.push_back(nums[i]);      // Choose the element
-            backtrack(nums, i + 1, current, result); // Move to next element
-            current.pop_back();              // Backtrack (un-choose)
+
+class Solution
+{
+public:
+    void GetallSubsets(vector<int> &nums, vector<int> &ans, int i, vector<vector<int>> &allSubsets)
+    {
+        if (i == nums.size())
+        {
+            allSubsets.push_back({ans});
+            return;
         }
+        // includes
+        ans.push_back(nums[i]);
+        GetallSubsets(nums, ans, i + 1, allSubsets);
+
+        ans.pop_back();
+        // exclude
+        GetallSubsets(nums, ans, i + 1, allSubsets);
     }
 
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int> current;
-        // Pre-allocate memory to avoid reallocations
-        result.reserve(1 << nums.size());
-        backtrack(nums, 0, current, result);
-        return result;
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        vector<vector<int>> allSubsets;
+        vector<int> ans;
+        GetallSubsets(nums, ans, 0, allSubsets);
+
+        return allSubsets;
     }
 };
