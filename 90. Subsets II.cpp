542ea7ca0742362dc9736fb5f4569224cac8dfc3
Ajit Dev <ajitk23192@gmail.com>
2026-05-22 10:55:20 +0530

    Refactor subsetsWithDup and add getallSubsets method

--- 90. Subsets II.cpp
+++ 90. Subsets II.cpp
@@ -1,46 +1,41 @@
+#include <iostream>
 #include <vector>
-#include <algorithm>
-
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        // Sort to bring duplicates together for easy pruning
+    void getallSubsets(vector<int> &nums, vector<int> &ans, int i, vector<vector<int>> &allSubsets)
+    {
+        if (i == nums.size())
+        {
+            allSubsets.push_back(ans);
+            return;
+        }
+
+        // includes
+        ans.push_back(nums[i]);
+        getallSubsets(nums, ans, i + 1, allSubsets);
+
+        ans.pop_back();
+
+        int idx = i + 1;
+        while (idx < nums.size() && nums[idx] == nums[idx - 1])
+        {
+            idx++;
+        }
+        // excliding
+        getallSubsets(nums, ans, idx, allSubsets);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int> &nums)
+    {
         sort(nums.begin(), nums.end());
 
-        vector<vector<int>> result;
-        // Pre-allocate for the worst-case (2^n) to prevent reallocations
-        result.reserve(1 << nums.size());
+        vector<vector<int>>
+            allSubsets;
+        vector<int> ans;
 
-        vector<int> current;
-        current.reserve(nums.size());
-
-        backtrack(nums, 0, current, result);
-        return result;
-    }
-
-private:
-    void backtrack(const vector<int>& nums, int start, vector<int>& current, vector<vector<int>>& result) {
-        // Add the current subset to result (makes a copy)
-        result.push_back(current);
-
-        for (int i = start; i < nums.size(); ++i) {
-            // Pruning: skip duplicates at the same recursion level
-            if (i > start && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            current.push_back(nums[i]);
-            backtrack(nums, i + 1, current, result);
-            current.pop_back(); // Backtrack
-        }
+        getallSubsets(nums, ans, 0, allSubsets);
+        return allSubsets;
     }
 };
-
-// Global Fast I/O
-static const auto fast_io = []() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    return 0;
-}();
