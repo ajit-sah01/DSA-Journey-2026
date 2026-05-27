cb5df2c463a6eba22148788c58b3fadfacc9b62b
Ajit Dev <ajitk23192@gmail.com>
2026-05-27 12:36:25 +0530

    Refactor palindrome partitioning logic

--- 131. Palindrome Partitioning.cpp
+++ 131. Palindrome Partitioning.cpp
@@ -1,45 +1,47 @@
-class Solution {
+
+class Solution
+{
 public:
-    vector<vector<string>> partition(string s) {
-        int n = s.length();
-        // 1. Bitset or vector<vector<bool>> for DP.
-        // vector<deque> or vector<vector<char>> is sometimes faster due to bit packing.
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
+    //// Check PaliDrome
+    bool isPali(string s)
+    {
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
 
-        for (int right = 0; right < n; right++) {
-            for (int left = 0; left <= right; left++) {
-                if (s[left] == s[right] && (right - left <= 2 || dp[left + 1][right - 1])) {
-                    dp[left][right] = true;
-                }
-            }
-        }
-
-        vector<vector<string>> result;
-        vector<string> currentPath;
-        // Optimization: Reserve space to avoid multiple reallocations
-        currentPath.reserve(n);
-
-        dfs(s, 0, dp, currentPath, result);
-        return result;
+        return s == s2;
     }
 
-private:
-    void dfs(const string& s, int start, const vector<vector<bool>>& dp,
-             vector<string>& currentPath, vector<vector<string>>& result) {
-        if (start == s.length()) {
-            result.push_back(currentPath);
+    //// Get all Parts
+    void getAllParts(string s,
+                     vector<string> &partitions,
+                     vector<vector<string>> &ans)
+    {
+        short n = s.size();
+        if (n == 0)
+        {
+            ans.push_back(partitions);
             return;
         }
+        for (int i = 0; i < n; i++)
+        {
+            string part = s.substr(0, i + 1);
 
-        for (int end = start; end < s.length(); end++) {
-            if (dp[start][end]) {
-                // We still use substr here, but only for valid palindromes.
-                // To go even faster, some use string_view (C++17),
-                // but LeetCode's result type is vector<string>.
-                currentPath.push_back(s.substr(start, end - start + 1));
-                dfs(s, end + 1, dp, currentPath, result);
-                currentPath.pop_back(); // Backtrack
+            if (isPali(part))
+            {
+                partitions.push_back(part);
+                getAllParts(s.substr(i + 1), partitions, ans);
+                partitions.pop_back();
             }
         }
+    }
+
+    vector<vector<string>> partition(string s)
+    {
+        vector<vector<string>> ans;
+        vector<string> partitions;
+
+        getAllParts(s, partitions, ans);
+
+        return ans;
     }
 };
