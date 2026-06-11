66bf737531f994aab161cb1b37a8481685f0a5cb
Ajit Dev <ajitk23192@gmail.com>
2026-06-11 07:12:43 +0530

    Update 1480. Running Sum of 1d Array.cpp
    // Prefix Sum Approach
    // Update each element with the sum of all previous elements.
    // nums[i] = nums[i] + nums[i-1]
    //
    // Time Complexity: O(n)
    // Space Complexity: O(1)

--- 1480. Running Sum of 1d Array.cpp
+++ 1480. Running Sum of 1d Array.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans (n,1);
-        ans[0] = nums[0];
+        // int n = nums.size();
+        // vector<int> ans (n,1);
+        // ans[0] = nums[0];
 
-        for(int i =1 ; i<n ; i++){
-           ans[i] = ans[i-1] + nums[i];
+        // for(int i =1 ; i<n ; i++){
+        //    ans[i] = ans[i-1] + nums[i];
+        // }
+        // return ans;
+
+        //// more better 
+
+        int n = nums.size();
+        for(int i =1; i<n ; i++){
+            nums[i] += nums[i-1];
         }
-        return ans;
+        return nums;
+        
     }
 };
