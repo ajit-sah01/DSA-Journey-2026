d2ac9938694a06de8282040ebf21f302e8e6961c
AJIT DEV <ajitk23192@gmail.com>
2026-07-05 10:40:44 +0530

    Time: 0 ms (100%), Space: 71.8 MB (51.97%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -13,16 +13,18 @@ public:
         // }
         // return maxSum;
 
-
         //// Kadane's Algorithm 
-        int currSum = 0 , maxSum = INT_MIN;
-        for(int i = 0 ; i<A.size(); i++){
-            currSum += A[i];
-            maxSum = max(currSum, maxSum);
-            if(currSum < 0){
-                currSum = 0;
-            }
-        }
-        return maxSum;
+        int currSum = 0, maxSum = INT_MIN;
+           for(int i = 0; i < A.size(); i++) {
+   	       currSum += A[i]; // Current subarray ka sum
+      	maxSum = max(currSum, maxSum); // Maximum update
+   	    if(currSum < 0) { // Agar loss ho raha hai
+   		currSum = 0; // Naya subarray start karo
+     	}
+         }
+       return maxSum;
+
+
+     
     }
 };
