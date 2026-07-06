8503bcefd26af35b70d5b6f0793e49262a89f59e
AJIT DEV <ajitk23192@gmail.com>
2026-07-06 11:37:52 +0530

    Time: 4 ms (28.48%), Space: 15.2 MB (60.13%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -27,13 +27,40 @@
 // };
 
 
+// class Solution {
+// public:
+//     int removeCoveredIntervals(vector<vector<int>>& intervals) {
+
+//         sort(intervals.begin(), intervals.end(),
+//              [](const vector<int>& a, const vector<int>& b) {
+//                  return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0];
+//              });
+
+//         int ans = 0;
+//         int maxEnd = -1;
+
+//         for (const auto& interval : intervals) {
+//             if (interval[1] > maxEnd) {
+//                 ++ans;
+//                 maxEnd = interval[1];
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-
         sort(intervals.begin(), intervals.end(),
-             [](const vector<int>& a, const vector<int>& b) {
-                 return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0];
+             [](const auto& a, const auto& b) {
+                 if (a[0] == b[0])
+                     return a[1] > b[1];
+                 return a[0] < b[0];
              });
 
         int ans = 0;
