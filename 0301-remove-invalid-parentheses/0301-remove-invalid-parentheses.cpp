d35d1f3378c753ae05234aa084643deaa7155d6c
AJIT DEV <ajitk23192@gmail.com>
2026-07-28 13:33:20 +0530

    Time: 127 ms (40.86%), Space: 12.7 MB (65.38%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
@@ -1,58 +1,68 @@
 class Solution {
 public:
-    unordered_set<string> ans;
+    vector<string> ans;
+    unordered_set<string> vis;
 
     void dfs(string &s, int idx, int leftRem, int rightRem,
-             int open, string path) {
+             int open, string &path) {
 
         if (idx == s.size()) {
-            if (leftRem == 0 && rightRem == 0 && open == 0)
-                ans.insert(path);
+            if (leftRem == 0 && rightRem == 0 && open == 0) {
+                if (vis.insert(path).second)
+                    ans.push_back(path);
+            }
             return;
         }
 
         char ch = s[idx];
 
-        // Letter
         if (isalpha(ch)) {
-            dfs(s, idx + 1, leftRem, rightRem, open, path + ch);
+            path.push_back(ch);
+            dfs(s, idx + 1, leftRem, rightRem, open, path);
+            path.pop_back();
             return;
         }
 
-        // Remove '('
+        // Remove current parenthesis
         if (ch == '(' && leftRem > 0)
             dfs(s, idx + 1, leftRem - 1, rightRem, open, path);
 
-        // Remove ')'
         if (ch == ')' && rightRem > 0)
             dfs(s, idx + 1, leftRem, rightRem - 1, open, path);
 
         // Keep '('
-        if (ch == '(')
-            dfs(s, idx + 1, leftRem, rightRem, open + 1, path + ch);
+        if (ch == '(') {
+            path.push_back(ch);
+            dfs(s, idx + 1, leftRem, rightRem, open + 1, path);
+            path.pop_back();
+        }
 
         // Keep ')'
-        if (ch == ')' && open > 0)
-            dfs(s, idx + 1, leftRem, rightRem, open - 1, path + ch);
+        if (ch == ')' && open > 0) {
+            path.push_back(ch);
+            dfs(s, idx + 1, leftRem, rightRem, open - 1, path);
+            path.pop_back();
+        }
     }
 
     vector<string> removeInvalidParentheses(string s) {
 
         int leftRem = 0, rightRem = 0;
 
-        for (char ch : s) {
-            if (ch == '(') {
+        for (char c : s) {
+            if (c == '(')
                 leftRem++;
-            } else if (ch == ')') {
-                if (leftRem > 0)
+            else if (c == ')') {
+                if (leftRem)
                     leftRem--;
                 else
                     rightRem++;
             }
         }
 
-        dfs(s, 0, leftRem, rightRem, 0, "");
+        string path;
+        dfs(s, 0, leftRem, rightRem, 0, path);
 
-        return vector<string>(ans.begin(), ans.end());
+        return ans;
     }
 };
