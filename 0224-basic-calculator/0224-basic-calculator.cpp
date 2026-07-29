cb2a0eae53df0b7f5a01d9d8dea5ce4c68d323d1
AJIT DEV <ajitk23192@gmail.com>
2026-07-29 12:00:56 +0530

    Time: 0 ms (100%), Space: 12.8 MB (63.35%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<long long> st;
+        long long result = 0;
+        long long num = 0;
+        long long sign = 1;
+
+        for (char c : s) {
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+            else if (c == '+') {
+                result += sign * num;
+                num = 0;
+                sign = 1;
+            }
+            else if (c == '-') {
+                result += sign * num;
+                num = 0;
+                sign = -1;
+            }
+            else if (c == '(') {
+                st.push(result);
+                st.push(sign);
+                result = 0;
+                sign = 1;
+            }
+            else if (c == ')') {
+                result += sign * num;
+                num = 0;
+
+                result *= st.top(); // sign before '('
+                st.pop();
+
+                result += st.top(); // result before '('
+                st.pop();
+            }
+        }
+
+        result += sign * num;
+        return (int)result;
+    }
+};
