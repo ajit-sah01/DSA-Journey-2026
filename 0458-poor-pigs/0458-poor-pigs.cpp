40fedb3dcefdfb8c453a4b32dce775f2a85c8dcc
AJIT DEV <ajitk23192@gmail.com>
2026-08-27 10:16:41 +0530

    Time: 0 ms (100%), Space: 7.8 MB (82.75%) - LeetHub

--- 0458-poor-pigs/0458-poor-pigs.cpp
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        // int states = minutestotest / minutestodie + 1;
-
-        // int pigs = 0;
-        // long long total = 1;
-
-        // while (total < buckets) {
-        //     total *= states;
-        //     pigs++;
-        // }
-
-        // return pigs;
-        
         int states = minutesToTest / minutesToDie + 1;
+
         int pigs = 0;
         long long combinations = 1;
 
         while (combinations < buckets) {
             combinations *= states;
-            ++pigs;
+            pigs++;
         }
 
         return pigs;
