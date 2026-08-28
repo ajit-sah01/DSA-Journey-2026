d44d305a0a89c5f376dc6d392c1af74cf47f19bb
AJIT DEV <ajitk23192@gmail.com>
2026-08-28 10:46:19 +0530

    Time: 0 ms (100%), Space: 14.9 MB (43.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,58 +1,32 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    //   // O(n2) Time Complexity ..
-    //   int n = nums.size();
-    //   for(int i = 0 ; i<n;i++){
-    //     for(int j = i+1 ; j<n;j++){
 
-    //         if(nums[i] + nums[j] == target){
-    //             return {i,j}; // if found ans return 
-    //         };
-    //     };
-    //   };
-    //   return {-1,-1}; // not find tha ans
+        // Brute Force Approach
+        // int n = nums.size();
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = i + 1; j < n; j++) {
+        //         if (nums[i] + nums[j] == target) {
+        //             return {i, j};
+        //         }
+        //     }
+        // }
+        // return {};
 
+        // Optimized Approach - Unordered Map
+        unordered_map<int, int> mp;
 
-    //  //   // O(n log n ) Time Complexity .. but not work proper !
-    //     sort(nums.begin(), nums.end());
-    //     int st = 0, end = nums.size() - 1;
-    //     while (st < end)
-    //     {
-    //         if (nums[st] + nums[end] == target)
-    //         {
-    //             return {st, end};
-    //         }
-    //         else if (nums[st] + nums[end] > target)
-    //         {
-    //             end--;
-    //         }
-    //         else
-    //         {
-    //             st++;
-    //         }
-    //     }
-    //     return {-1,-1};
+        for (int i = 0; i < nums.size(); i++) {
 
+            int need = target - nums[i];
 
-        unordered_map<int, int> m;
-        vector<int> ans;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            int first = nums[i];
-            int sec = target - first;
-
-            if (m.find(sec) != m.end())
-            {
-                ans.push_back(i);
-                ans.push_back(m[sec]);
-                break;
+            if (mp.find(need) != mp.end()) {
+                return {mp[need], i};
             }
-            m[first] = i;
+
+            mp[nums[i]] = i;
         }
-        return ans;
 
-
-
-    };
+        return {};
+    }
 };
