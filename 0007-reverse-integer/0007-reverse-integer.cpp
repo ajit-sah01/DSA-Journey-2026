d78e7626184cdedac7c1e66b21b4af195ec5d1cc
AJIT DEV <ajitk23192@gmail.com>
2026-08-29 11:11:32 +0530

    Time: 2 ms (38.44%), Space: 8.6 MB (20.44%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,21 +1,49 @@
-class Solution
-{
-public:
-    int reverse(int n)
-    {
-        int reverseNum = 0;
-        while (n != 0)
-        {
-            int dig = n % 10;
+// class Solution
+// {
+// public:
+//     int reverse(int n)
+//     {
+//         int reverseNum = 0;
+//         while (n != 0)
+//         {
+//             int dig = n % 10;
 
-            if(reverseNum > INT_MAX/10 || reverseNum < INT_MIN/10){
+//             if(reverseNum > INT_MAX/10 || reverseNum < INT_MIN/10){
+//                 return 0;
+//             }
+
+//             reverseNum = reverseNum * 10 + dig;
+
+//             n = n / 10;
+//         }
+//         return reverseNum;
+//     }
+// };
+
+
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            // Check overflow before rev * 10 + digit
+            if (rev > INT_MAX / 10 ||
+                (rev == INT_MAX / 10 && digit > 7)) {
                 return 0;
             }
 
-            reverseNum = reverseNum * 10 + dig;
+            if (rev < INT_MIN / 10 ||
+                (rev == INT_MIN / 10 && digit < -8)) {
+                return 0;
+            }
 
-            n = n / 10;
+            rev = rev * 10 + digit;
         }
-        return reverseNum;
+
+        return rev;
     }
 };
