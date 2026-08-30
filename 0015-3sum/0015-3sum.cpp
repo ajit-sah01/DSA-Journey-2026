b1886056dd9ffd1519feaf40de01bb7cca6f0b00
AJIT DEV <ajitk23192@gmail.com>
2026-08-30 10:24:01 +0530

    Time: 51 ms (41.69%), Space: 29.2 MB (46.16%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,115 @@
+
+
+// class Solution
+// {
+// public:
+    // //// Brutforce Appocch
+    // vector<vector<int>> threeSum(vector<int> &nums)
+    // {
+    //     int n = nums.size()  ;
+    //     set<vector<int>> s;      // set unique Triplets Store
+    //     vector<vector<int>> ans; // set unique Triplets Store
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         for (int j = i + 1; j < n; j++)
+    //         {
+    //             for (int k = j + 1; k < n; k++)
+    //             {
+    //                 if ((nums[i] + nums[j] + nums[k]) == 0)
+    //                 {
+    //                     vector<int> trip = {nums[i],
+    //                                         nums[j],
+    //                                         nums[k]};
+    //                     sort(trip.begin(), trip.end());
+    //                     if (s.find(trip) == s.end())
+    //                     {
+    //                         s.insert(trip);
+    //                         ans.push_back(trip);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
+// };
+
+
+
+
+// class Solution
+// {
+// public:
+//     vector<vector<int>> threeSum(vector<int> &nums)
+//     {
+//         int n = nums.size();
+
+//         set<vector<int>> uniqueTrip;
+
+
+//         for (int i = 0; i < n; i++)
+//         {
+//             int tar = -nums[i];
+//             set<int> s;
+
+//             for (int j = i + 1; j < n; j++)
+//             {
+//                 int third = tar - nums[j];
+//                 if (s.find(third) != s.end())
+//                 {
+//                     vector<int> trip = {nums[i], nums[j], third};
+//                     sort(trip.begin(), trip.end());
+//                     uniqueTrip.insert(trip);
+//                 }
+//                 s.insert(nums[j]);
+//             }
+//         }
+//         vector<vector<int>> ans(uniqueTrip.begin(), uniqueTrip.end());
+//         return ans;
+//     }
+// }
+
+
+
+
+// better Appooch but silii TLE Time Com O(n * lon n) Otimized
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int st = 0, end = n;
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int j = i + 1, k = n - 1;
+            while (j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum > 0)
+                {
+                    k--;
+                }
+                else if (sum < 0)
+                {
+                    j++;
+                }
+                else
+                {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                }
+            }
+        }
+        return ans;
+    }
+};
