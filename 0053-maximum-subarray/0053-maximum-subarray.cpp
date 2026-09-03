5a770871bf86fd30a23661f0e6439eec720ca52f
AJIT DEV <ajitk23192@gmail.com>
2026-09-03 10:12:27 +0530

    Time: 0 ms (100%), Space: 71.8 MB (14.9%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,40 +1,18 @@
-class Solution
-{
+class Solution {
 public:
-    int maxSubArray(vector<int> &A)
-    {
+    int maxSubArray(vector<int>& nums) {
+        int currSum = 0 , maxSum = INT_MIN , n = nums.size()-1;
 
-        // //// Kadane's Algorithm
-        // int currSum = 0 , maxSum = INT_MIN;
-        // for(int i = 0; i<A.size(); i++){
-        //     currSum += A[i];
-        //     maxSum = max(maxSum, currSum);
-        //     if(currSum < 0){
-        //         currSum = 0;
-        //     }
-        // }
-        // return maxSum;
+        for(int i = 0; i<= n ; i++ ){
+            currSum += nums[i];
+            maxSum = max(maxSum,currSum);
 
-        //     //// Kadane's Algorithm
-        //     int currSum = 0, maxSum = INT_MIN;
-        //        for(int i = 0; i < A.size(); i++) {
-        //        currSum += A[i]; // Current subarray ka sum
-        //   	maxSum = max(currSum, maxSum); // Maximum update
-        //     if(currSum < 0) { // Agar loss ho raha hai
-        // 	currSum = 0; // Naya subarray start karo
-        //  	}
-        //      }
-        //    return maxSum;
-
-        //// Kadane's Algorithm
-        int currSum  = 0  , maxSum = INT_MIN;
-        for(int val : A){
-           currSum += val;
-           maxSum = max(maxSum , currSum);
-           if(currSum < 0){
-              currSum = 0;
+            if(currSum < 0){
+                currSum = 0;
             }
         }
+
         return maxSum;
+
     }
 };
