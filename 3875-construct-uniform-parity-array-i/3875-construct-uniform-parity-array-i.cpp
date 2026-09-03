a80c440c5b152f05cf9b49f7115c0ee3659f7150
AJIT DEV <ajitk23192@gmail.com>
2026-09-03 11:55:11 +0530

    Time: 0 ms (100%), Space: 30.3 MB (36.41%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
-    bool uniformArray(vector<int>& nums) {
-        // int mn = INT_MAX;
-
-        // for (int x : nums)
-        //     mn = min(mn, x);
-
-        // if (mn & 1)
-        //     return true;
-
-        // for (int x : nums)
-        //     if ((x & 1) != (nums[0] & 1))
-        //         return false;
-
+    bool uniformArray(vector<int>& nums) {       
         return true;
     }
 };
